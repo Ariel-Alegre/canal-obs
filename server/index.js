4816243abafca2eaae8c154b8b1c22022dc52249
Ariel-Alegre <arielalegre98@gmail.com>
2024-09-09 23:30:16 -0300

    update

--- server/index.js
+++ server/index.js
@@ -57,8 +57,7 @@ nms.on('prePublish', (id, streamPath, args) => {
 });
 
 const app = express();
-const port = 3001;
-
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use('/slv', express.static(slvPath));
 
